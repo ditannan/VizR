dd0092deda8900c9f5203e2f2de34f529b0b0e97
Delta <xzm9210@gmail.com>
2018-02-11 13:15:06 +0800

    mapping continuous variables to colour or shape

--- Chapter5_Scatter.R
+++ Chapter5_Scatter.R
@@ -21,3 +21,36 @@ hw %>%
   geom_point() +
   scale_shape_manual(values = c(21, 24)) +
   scale_fill_manual(values = c(NA, 'black'), guide = guide_legend(override.aes = list(shape = 21)))
+
+# mapping continuous variable to colour or shape --------------------------
+
+heightweight %>% 
+  ggplot(aes(ageYear, heightIn, colour = weightLb, fill = weightLb)) +
+  geom_point(shape = 22, size = 2.5) +
+  scale_color_gradient(low = 'black', high = 'white') +
+  scale_fill_gradient(low = 'black', high = 'white')
+heightweight %>% 
+  ggplot(aes(ageYear, heightIn, size = weightLb)) +
+  geom_point() +
+  scale_size_continuous(range = c(2, 5))
+heightweight %>% 
+  ggplot(aes(ageYear, heightIn, fill = weightLb)) +
+  geom_point(shape = 21, size = 2.5) +
+  scale_fill_gradient(low = 'black', high = 'white', breaks = seq(70, 170, by = 20), guide = guide_legend())
+  
+heightweight %>% 
+  ggplot(aes(ageYear, heightIn, size = weightLb, colour = sex)) +
+  geom_point(alpha = .5) +
+  scale_size_area() +
+  scale_color_brewer(palette = 'Set1')
+
+
+
+
+
+
+
+
+
+
+
