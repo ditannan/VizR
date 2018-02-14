a3c58eaed6fa09dd569a53de2b219a03d6a466b5
Delta <xzm9210@gmail.com>
2018-02-14 13:50:33 +0800

    set range for continuous axis

--- Chapter8_Axis.R
+++ Chapter8_Axis.R
@@ -19,4 +19,12 @@ PlantGrowth %>%
 
 # set range for continuous axis -------------------------------------------
 
-
+# xlim(), ylim()
+p <- PlantGrowth %>% 
+  ggplot(aes(group, weight)) +
+  geom_boxplot()
+p + ylim(0, max(PlantGrowth$weight)) 
+## ylim(0, 10) equivalent to scale_y_continuous(limits = c(0, 10))
+p + scale_y_continuous(limits = c(5, 6.5))
+p + coord_cartesian(ylim = c(5, 6.5))
+p + expand_limits(y = 0)
